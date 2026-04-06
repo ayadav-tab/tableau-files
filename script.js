a3b2c3bbcb2a6997647eb390fce7e7377127234f
ayadav-tab <ayadav@breakthrought1d.org>
2026-04-06 21:11:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-$(document).ready(function () {
-console.log('Running');
-});
+(function () {
+  $(document).ready(function () {
+    tableau.extensions.initializeAsync().then(function () {
+      const dashboard = tableau.extensions.dashboardContent.dashboard;
+    const worksheet = dashboard.worksheets[0];
+
+    worksheet.getSummaryDataAsync().then(function (data) {
+        let html = "<table><tr>";
+
+        data.columns.forEach(col => {
+            html += "<th>" + col.fieldName + "</th>";
+        });
+
+        html += "<th>Link</th></tr>";
+
+        data.data.forEach(row => {
+            html += "<tr>";
+
+            row.forEach(cell => {
+                html += "<td>" + cell.formattedValue + "</td>";
+            });
+
+            html += "<td class='link'><a href='https://www.google.com'/></td>";
+            html += "</tr>";
+        });
+
+        html += "</table>";
+        document.getElementById("tableContainer").innerHTML = html;
+    });
+    }, function (err) {
+      // Something went wrong in initialization.
+      console.log('Error while Initializing: ' + err.toString());
+    });
+  });
+})();
